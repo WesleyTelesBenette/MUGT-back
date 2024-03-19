d216a62a274516742a9dc5b2de9e4de26cd66676
Wesley T. Benette <weluonf@gmail.com>
2024-03-18 23:02:54 -0300

    Criação do Controller.

---
+++ Controllers/GenerateUserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mugt_back.Dtos;
+using mugt_back.Services;
+
+namespace mugt_back.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GenerateUserController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult GenerateUser([FromBody] GenerateUserRequestDto bodyObject)
+    {
+        if (bodyObject == null)
+        {
+            return BadRequest();
+        }
+
+        GenerateUserResponseDto responseObject = GenerateService.Start(bodyObject);
+        return Ok(responseObject);
+    }
+}
