468805b54212132ed77d45cf535ea267e0381b83
Wesley T. Benette <weluonf@gmail.com>
2024-03-18 23:02:37 -0300

    Criação dos DTOs de Resquest e Response.

---
+++ Dtos/GenerateUserRequestDto.cs
@@ -0,0 +1,31 @@
+﻿namespace mugt_back.Dtos;
+
+public class GenerateUserRequestDto
+{
+    public bool Name { get; set; } = false;
+    public bool UserName { get; set; } = false;
+    public bool Age { get; set; } = false;
+    public bool Birthday { get; set; } = false;
+    public bool Ethnicity { get; set; } = false;
+    public bool Nationality { get; set; } = false;
+    public bool Email { get; set; } = false;
+    public bool Telephone { get; set; } = false;
+    public bool PhysicalDisability { get; set; } = false;
+    public bool VoterId { get; set; } = false;
+    public bool DriversLicense { get; set; } = false;
+    public bool MaritalStatus { get; set; } = false;
+    public bool ProfileImage { get; set; } = false;
+    public bool EnemScore { get; set; } = false;
+    public bool AcademicEducation { get; set; } = false;
+    public bool Profession { get; set; } = false;
+    public bool PsychologicalProblem { get; set; } = false;
+    public bool BasicPassword { get; set; } = false;
+    public bool StrongPassword { get; set; } = false;
+    public bool CPF { get; set; } = false;
+    public bool RG { get; set; } = false;
+    public bool Gender { get; set; } = false;
+    public bool FavouriteColour { get; set; } = false;
+    public bool FavoriteFood { get; set; } = false;
+    public bool Weight { get; set; } = false;
+    public bool Height { get; set; } = false;
+}
---
+++ Dtos/GenerateUserResponseDto.cs
@@ -0,0 +1,31 @@
+﻿namespace mugt_back.Dtos;
+
+public class GenerateUserResponseDto
+{
+    public string? Name { get; set; } = null;
+    public string? UserName { get; set; } = null;
+    public int? Age { get; set; } = null;
+    public string? Birthday { get; set; } = null;
+    public string? Ethnicity { get; set; } = null;
+    public string? Nationality { get; set; } = null;
+    public string? Email { get; set; } = null;
+    public string? Telephone { get; set; } = null;
+    public string? PhysicalDisability { get; set; } = null;
+    public string? VoterId { get; set; } = null;
+    public string? DriversLicense { get; set; } = null;
+    public string? MaritalStatus { get; set; } = null;
+    public string? ProfileImage { get; set; } = null;
+    public string? EnemScore { get; set; } = null;
+    public string? AcademicEducation { get; set; } = null;
+    public string? Profession { get; set; } = null;
+    public string? PsychologicalProblem { get; set; } = null;
+    public string? BasicPassword { get; set; } = null;
+    public string? StrongPassword { get; set; } = null;
+    public string? CPF { get; set; } = null;
+    public string? RG { get; set; } = null;
+    public string? Gender { get; set; } = null;
+    public string? FavouriteColour { get; set; } = null;
+    public string? FavoriteFood { get; set; } = null;
+    public double? Weight { get; set; } = null;
+    public double? Height { get; set; } = null;
+}
